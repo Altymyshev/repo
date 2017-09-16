128b4cdcd7b43e3909d1d75db33178bfa1f48ae9
Руслан Алтымышев <altymyshev@reg.ru>
2017-09-16 23:53:24 +0400

    gl6.

--- test.pl
+++ test.pl
@@ -1,36 +1,19 @@
 #!/usr/bin/perl
+#use Modern::Perl;
 use strict;
-use 5.010;
+=pod
+print "Введи кол-во столбцов: ";
+chomp (my $stolb = <STDIN>);
+chomp (my @arr_String = qw /hello good-bye/);
+my $format = "%${stolb}s\n";
+print "123456789012345678901234567890123456789012345678901234567890\n";
+foreach (@arr_String){
 
-
-sub above_average(){
-
-sub total(){
-	my @number = @_;
-	my $sum = 0;
-	foreach $_ (@number){
-		$sum = $sum + $_;
-	}
-	my $average_total = $sum/@number;
-	return $average_total;
+	printf "%${stolb}s\n", $_;
+	
 }
-
-my @itog;
-my @top = @_;
-my $average = &total(@top);
-foreach $_ (@top){
-	if($_ > $average){
-		push (@itog, $_);
-	}
-}
-	return @itog;
-}
-
-#my @fred = &above_average(1..10);
-#print "\@fred is @fred\n";
-#print "(Should be 6 7 8 9 10)\n";
-#my @barney = &above_average(100, 1..10);
-#print "\@barney is @barney\n";
-#print "(Should be just 100)\n";
-
-sub above_average(){}
+=cut
+my %hash_Name = {"fred" => "flinstone", "barney" => "rubble", "wilma" => "flinstone"};
+print "Say name:\n";
+chomp(my $say_Name = <STDIN>);
+print $hash_Name{$say_Name};
